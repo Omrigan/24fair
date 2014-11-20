8c11f3e8a16911e8ed3cd29eac7bd5e5c9a04b95
Omrigan <omrigann@gmail.com>
2014-11-20 09:20:04 +0700

    Mobile presentation fixed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function (){
     setInterval(function (){
-        timespan = countdown(new Date(2014,11,13,13));
+        timespan = countdown( new Date(2014,11,13,13), null, countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS);
         $('#days').html(timespan.days.toString());
         $('#hours').html(timespan.hours.toString());
         $('#minutes').html(timespan.minutes.toString());
